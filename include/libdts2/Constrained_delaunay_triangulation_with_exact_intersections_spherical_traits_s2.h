24ae4f41fe5328974881905f0f1ece86f740d7fc
Daniel Bahrdt <daniel.git@funroll-loops.de>
2017-01-07 14:33:12 +0100

    begin implementation of spherical kernel support

--- include/libdts2/Constrained_delaunay_triangulation_with_exact_intersections_spherical_traits_s2.h
+++ include/libdts2/Constrained_delaunay_triangulation_with_exact_intersections_spherical_traits_s2.h
@@ -3,54 +3,85 @@
 #pragma once
 
 #include <libdts2/constants.h>
+
 #include <CGAL/Exact_spherical_kernel_3.h>
 
+#include <libratss/Conversion.h>
+#include <libdts2/Constrained_delaunay_triangulation_base_traits_s2.h>
 
 namespace LIB_DTS2_NAMESPACE {
 
 class Constrained_delaunay_triangulation_with_exact_intersections_spherical_traits_s2 {
 public:
-	using MyBaseTrait = CGAL::Exact_spherical_kernel_3;
-protected:
-	using Compare_distance_3 = MyBaseTrait::Linear_kernel::Compare_distance_3;
+	using MyKernel = CGAL::Exact_spherical_kernel_3;
+	using MyLinearCDTKernel = Constrained_delaunay_triangulation_base_traits_s2<MyKernel::Linear_kernel>;
 public:
-	using FT = MyBaseTrait::FT;
-	using Oriented_side = MyBaseTrait::Oriented_side;
-	using Comparison_result = MyBaseTrait::Comparison_result;
-	using Orientation = MyBaseTrait::Orientation;
+	using FT = MyKernel::FT;
+	using Oriented_side = MyKernel::Oriented_side;
+	using Comparison_result = MyKernel::Comparison_result;
+	using Orientation = MyKernel::Orientation;
 	
-	using Segment_2 = MyBaseTrait::Circular_arc_3;
+	using Segment_2 = MyKernel::Circular_arc_3;
 	using Segment = Segment_2;
 	
-	//we want our triangulation to live in cartesian 3D-space
-	//the CDT lives in 2D euclidean space so we have to redefine Point_2;
-	using Point_3 = MyBaseTrait::Point_3;
-	using Point_2 = Point_3;
-	using Point = Point_2;
+	using Point_3 = MyLinearCDTKernel::Point_3;
+	using Point_2 = MyLinearCDTKernel::Point_2;
+	using Point = MyLinearCDTKernel::Point;
 	
-	using Compare_distance_2 = Compare_distance_3;
+	using Compare_distance_2 = MyLinearCDTKernel::Compare_distance_2;
 	
 	//the following is needed for spatial sorting
-	using Less_x_3 = MyBaseTrait::Linear_kernel::Less_x_3;
-	using Less_y_3 = MyBaseTrait::Linear_kernel::Less_y_3;
-	using Less_z_3 = MyBaseTrait::Linear_kernel::Less_z_3;
+	using Less_x_3 = MyLinearCDTKernel::Less_x_3;
+	using Less_y_3 = MyLinearCDTKernel::Less_y_3;
+	using Less_z_3 = MyLinearCDTKernel::Less_z_3;
 	
 	//this is needed for spatial sorting during insertion of constraints
 	//cgal uses Spatial_sort_traits_adapter_2 to sort inserted vertices
 	//this in turn needs the following predicates
 	//maybe we should just specialize Spatial_sort_traits_adapter_2 for this Trait
 	
-	using Less_x_2 = Less_x_3;
-	using Less_y_2 = Less_y_3;
+	using Less_x_2 = MyLinearCDTKernel::Less_x_2;
+	using Less_y_2 = MyLinearCDTKernel::Less_y_2;
 	
-	using Construct_segment_2 = MyBaseTrait::Construct_circular_arc_3;
+	using Construct_segment_2 = MyKernel::Construct_circular_arc_3;
 public:
-	Constrained_delaunay_triangulation_with_inexact_intersections_spherical_traits_s2();
+	class Project_on_sphere: public MyLinearCDTKernel::Project_on_sphere {
+	public:
+		using MyBaseClass = MyLinearCDTKernel::Project_on_sphere;
+	private:
+		template<typename T>
+		using Conversion = LIB_RATSS_NAMESPACE::Conversion<T>;
+	public:
+		Project_on_sphere(const MyBaseClass & v) : MyBaseClass(v) {}
+	public:
+		using MyBaseClass::operator();
+		Point_3 operator()(const Point_3 & v) const {
+			FT sqLen(v.x()*v.x() + v.y()*v.y() + v.z()*v.z());
+			if (sqLen == 1) {
+				return v;
+			}
+			
+			mpq_class sqLenQ( Conversion<FT>::toMpq(sqLen) );
+			std::size_t sqLenPrec = projector().calc().maxBitCount(sqLenQ);
+			mpfr::mpreal sqLenF(Conversion<mpq_class>::toMpreal(sqLenQ, sqLenPrec));
+			mpfr::mpreal lenF = projector().calc().sqrt(sqLenF);
+			mpfr::mpreal xf(Conversion<FT>::toMpreal(v.x(), sqLenPrec));
+			mpfr::mpreal yf(Conversion<FT>::toMpreal(v.y(), sqLenPrec));
+			mpfr::mpreal zf(Conversion<FT>::toMpreal(v.z(), sqLenPrec));
+			mpq_class xq, yq, zq;
+			projector().snap(xf, yf, zf, xq, yq, zq, precision());
+			return Point_3( Conversion<FT>::moveFrom(xq),
+							Conversion<FT>::moveFrom(yq),
+							Conversion<FT>::moveFrom(zq)
+			);
+		}
+	};
+public:
+	Constrained_delaunay_triangulation_with_exact_intersections_spherical_traits_s2();
 private:
-	
-private:
-	MyBaseTrait m_traits;
+	   MyKernel m_traits;
 };
+
 
 }//end namespace
 
