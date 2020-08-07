7a7e186de12343b93c652eeb07efa5928b663944
Daniel Bahrdt <daniel.git@funroll-loops.de>
2020-08-07 17:17:24 +0200

    Point_sp: speed-up computation of Point_3 and x,y,z coordinates

--- include/libdts2/Kernel_sp/Point_sp.h
+++ include/libdts2/Kernel_sp/Point_sp.h
@@ -80,11 +80,13 @@ public:
 public:
 	Point_3 point3() const;
 public:
-	FT x() const { return point3().x(); }
-	FT y() const { return point3().y(); }
-	FT z() const { return point3().z(); }
+	FT x() const;
+	FT y() const;
+	FT z() const;
 public:
 	operator Point_3() const { return point3(); }
+private:
+	Point_3 point3_slow() const;
 };
 
 template<typename K1, typename K2>
@@ -161,6 +163,9 @@ PTSP_CLS_NAME::Point_sp(FT const & x, FT const & y, FT const & z) {
 		throw std::runtime_error("Point_sp: Position is out of bounds");
 	}
 	assert(Point_3(x,y,z) == point3());
+	assert(this->x() == x);
+	assert(this->y() == y);
+	assert(this->z() == z);
 }
 
 PTSP_TMP_PRMS
@@ -179,6 +184,40 @@ PTSP_CLS_NAME::point3() const {
 	if (pos() == 0) {  //Hack for LIB_DTS2_ORIGIN 
 		return LIB_DTS2_ORIGIN;
 	}
+	if (exponent() > 31) {
+		return point3_slow();
+	}
+	using LIB_RATSS_NAMESPACE::convert;
+	mpq_class x, y, z;
+	auto sqr = [](int64_t v) { return v*v; };
+	int64_t den2 = sqr(denominator());
+	int64_t sum_p_i2 = sqr(numerator0()) + sqr(numerator1());
+	switch (abs(pos())) {
+	case 1: //x is the missing coordinate, thus num0 holds y, num1 holds z
+		x = (std::signbit<int>(pos()) ? 1 : -1)*mpq_class(sum_p_i2 - den2)/mpq_class(den2 + sum_p_i2);
+		y = mpq_class(2*numerator0()*denominator())/mpq_class(den2 + sum_p_i2);
+		z = mpq_class(2*numerator1()*denominator())/mpq_class(den2 + sum_p_i2);
+		break;
+	case 2: //y is the missing coordinate, thus num0 holds x, num1 holds z
+		x = mpq_class(2*numerator0()*denominator())/mpq_class(den2 + sum_p_i2);
+		y = (std::signbit<int>(pos()) ? 1 : -1)*mpq_class(sum_p_i2 - den2)/mpq_class(den2 + sum_p_i2);
+		z = mpq_class(2*numerator1()*denominator())/mpq_class(den2 + sum_p_i2);
+		break;
+	case 3: //z is the missing coordinate, thus num0 holds x, num1 holds y
+		x = mpq_class(2*numerator0()*denominator())/mpq_class(den2 + sum_p_i2);
+		y = mpq_class(2*numerator1()*denominator())/mpq_class(den2 + sum_p_i2);
+		z = (std::signbit<int>(pos()) ? 1 : -1)*mpq_class(sum_p_i2 - den2)/mpq_class(den2 + sum_p_i2);
+		break;
+	};
+	Point_3 result(convert<FT>(x), convert<FT>(y), convert<FT>(z));
+	assert(result == point3_slow());
+	return result;
+}
+
+
+PTSP_TMP_PRMS
+typename PTSP_CLS_NAME::Point_3
+PTSP_CLS_NAME::point3_slow() const {
 	
 	LIB_RATSS_NAMESPACE::ProjectS2 proj;
 	mpq_class xp, yp, zp;
@@ -208,6 +247,103 @@ PTSP_CLS_NAME::point3() const {
 	
 	using LIB_RATSS_NAMESPACE::convert;
 	return Point_3( convert<FT>(xs), convert<FT>(ys), convert<FT>(zs) );
+}
+
+//plane2sphere is as follows:
+// 2x_i/(1 + sum x_i^2)
+//=2(p_i/den)/(1+1/den^2 * sum p_i^2)
+//=2(p_i/den)/(1/den^2*(den^2 + sum p_i^2)
+//=2*p_i*den/(den^2 + sum p_i^2)
+//
+//Or for the removed dimension
+// (sum x_i^2 - 1)/(1 + sum x_i^2)
+//=(1/den^2*(sum p_i^2 - den^2))/(1/den^2*(den^2 + sum p_i^2))
+//=(sum p_i^2 - den^2)/(den^2 + sum p_i^2)
+//We have that {den, p_i} <= 2^31
+//Thus den^2 <= 2^62 and den^2 + den^2 <= 2^63
+//Hence int64_t should be enough
+
+
+PTSP_TMP_PRMS
+typename PTSP_CLS_NAME::FT
+PTSP_CLS_NAME::x() const {
+	if (pos() == 0) {  //Hack for LIB_DTS2_ORIGIN 
+		return FT(LIB_DTS2_ORIGIN_X);
+	}
+	if (exponent() > 31) {
+		return point3().x();
+	}
+	using LIB_RATSS_NAMESPACE::convert;
+	auto sqr = [](int64_t v) { return v*v; };
+	int64_t den2 = sqr(denominator());
+	int64_t sum_p_i2 = sqr(numerator0()) + sqr(numerator1());
+	mpq_class result;
+	switch (abs(pos())) {
+	case 1: //x is the missing coordinate
+		result = (std::signbit<int>(pos()) ? 1 : -1)*mpq_class(sum_p_i2 - den2)/mpq_class(den2 + sum_p_i2);
+		break;
+	default: //in any case num0 holds x
+		result = mpq_class(2*numerator0()*denominator())/mpq_class(den2 + sum_p_i2);
+		break;
+	};
+	assert(result == convert<mpq_class>(point3().x()));
+	return convert<FT>(result);
+}
+
+PTSP_TMP_PRMS
+typename PTSP_CLS_NAME::FT
+PTSP_CLS_NAME::y() const {
+	if (pos() == 0) {  //Hack for LIB_DTS2_ORIGIN 
+		return FT(LIB_DTS2_ORIGIN_Y);
+	}
+	if (exponent() > 31) {
+		return point3().y();
+	}
+	using LIB_RATSS_NAMESPACE::convert;
+	auto sqr = [](int64_t v) { return v*v; };
+	int64_t den2 = sqr(denominator());
+	int64_t sum_p_i2 = sqr(numerator0()) + sqr(numerator1());
+	mpq_class result;
+	switch (abs(pos())) {
+	case 1: //x is the missing coordinate, thus num0 holds y
+		result = mpq_class(2*numerator0()*denominator())/mpq_class(den2 + sum_p_i2);
+		break;
+	case 2: //y is the missing coordinate
+		result = (std::signbit<int>(pos()) ? 1 : -1)*mpq_class(sum_p_i2 - den2)/mpq_class(den2 + sum_p_i2);
+		break;
+	case 3: //z is the missing coordinate, thus num1 holds y
+		result = mpq_class(2*numerator1()*denominator())/mpq_class(den2 + sum_p_i2);
+		break;
+	};
+	assert(result == convert<mpq_class>(point3().y()));
+	return convert<FT>(result);
+}
+
+PTSP_TMP_PRMS
+typename PTSP_CLS_NAME::FT
+PTSP_CLS_NAME::z() const {
+	if (pos() == 0) {  //Hack for LIB_DTS2_ORIGIN 
+		return FT(LIB_DTS2_ORIGIN_Z);
+	}
+	if (exponent() > 31) {
+		return point3().z();
+	}
+	using LIB_RATSS_NAMESPACE::convert;
+	auto sqr = [](int64_t v) { return v*v; };
+	int64_t den2 = sqr(denominator());
+	int64_t sum_p_i2 = sqr(numerator0()) + sqr(numerator1());
+	mpq_class result;
+	switch (abs(pos())) {
+	case 1: //x,y are the missing coordinate, thus num1 holds z
+	case 2:
+		result = mpq_class(2*numerator1()*denominator())/mpq_class(den2 + sum_p_i2);
+		break;
+	case 3: //z is the missing coordinate, thus num1 holds y
+		result = (std::signbit<int>(pos()) ? 1 : -1)*mpq_class(sum_p_i2 - den2)/mpq_class(den2 + sum_p_i2);
+		break;
+	};
+	assert(result == convert<mpq_class>(point3().z()));
+	return convert<FT>(result);
 }
 
 #undef PTSP_TMP_PRMS
