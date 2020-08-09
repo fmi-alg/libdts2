1932e00530e673487f5eb43e081304f3867b041d
Daniel Bahrdt <daniel.git@funroll-loops.de>
2020-08-09 02:53:29 +0200

    Traits::Oriented_side_of_circle_2: make sure that we use the correct types when using the predicates from linear kernel

--- include/libdts2/Constrained_delaunay_triangulation_base_traits_s2.h
+++ include/libdts2/Constrained_delaunay_triangulation_base_traits_s2.h
@@ -16,7 +16,35 @@
 
 namespace LIB_DTS2_NAMESPACE {
 namespace detail {
-	
+
+///Identity pass through cast
+///This makes sure that casting the same class to the same class does not incur any overhead
+template<typename T_TARGET_TYPE, typename T_SOURCE_TYPE>
+struct ipt_static_cast_impl {
+	using SourceType = T_SOURCE_TYPE;
+	using TargetType = T_TARGET_TYPE;
+	static TargetType c(SourceType const & s) { return static_cast<TargetType>(s); }
+	static TargetType c(SourceType & s) { return static_cast<TargetType>(s); }
+};
+
+template<typename T_TARGET_TYPE>
+struct ipt_static_cast_impl<T_TARGET_TYPE, T_TARGET_TYPE> {
+	using SourceType = T_TARGET_TYPE;
+	using TargetType = T_TARGET_TYPE;
+	static TargetType const & c(SourceType const & s) { return s; }
+	static TargetType & c(SourceType & s) { return s; }
+};
+
+template<typename T_TARGET_TYPE, typename T_SOURCE_TYPE>
+auto ipt_static_cast(T_SOURCE_TYPE const & v) {
+	return ipt_static_cast_impl<T_TARGET_TYPE, T_SOURCE_TYPE>::c(v);
+}
+
+template<typename T_TARGET_TYPE, typename T_SOURCE_TYPE>
+auto ipt_static_cast(T_SOURCE_TYPE & v) {
+	return ipt_static_cast_impl<T_TARGET_TYPE, T_SOURCE_TYPE>::c(v);
+}
+
 template<typename T_LINEAR_KERNEL>
 class EpsBasedAuxPoints {
 public:
@@ -359,8 +387,13 @@ public: //own implementations
 		Orientation operator()(const Point_3 & p, const Point_3 & q, const Point_3 & r, const Point_3 & s) const
 		{
 			assert(p != q && p != r && q != r && r != s);
-			auto oriented_side = ot3(p, q, r, s);
-			return oriented_side;
+			using BasePoint = typename LinearKernel::Point_3;
+			return ot3(
+						detail::ipt_static_cast<BasePoint>(p),
+						detail::ipt_static_cast<BasePoint>(q),
+						detail::ipt_static_cast<BasePoint>(r),
+						detail::ipt_static_cast<BasePoint>(s)
+				   );
 		}
 	private:
 		Orientation_3 ot3;
