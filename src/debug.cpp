7985fc7feae710215e9fa1aae75d0c4e18c2da63
Daniel Bahrdt <daniel.git@funroll-loops.de>
2020-08-12 10:42:47 +0200

    Improve debug printing

--- src/debug.cpp
+++ src/debug.cpp
@@ -6,29 +6,41 @@ extern "C" {
 	
 void dts2_debug_print_point_sp(LIB_DTS2_NAMESPACE::Constrained_delaunay_triangulation_with_inexact_intersections_traits_s2_sp::Point_2 const & p) {
 	using ratss::convert;
-	std::cerr << "Point_sp(num0=" << p.numerator0() << ", num1=" << p.numerator1() << ", den=" << p.denominator() << ", pos=" << p.pos() << ")=";
+	std::cerr << "Point_sp(" << p.numerator0() << ", " << p.numerator1() << ", " << p.denominator() << ", " << p.pos() << ")=";
 	std::cerr << "Point_3(" << std::endl;
 	std::cerr << "x=";
 	ratss_debug_print_mpq_class( convert<mpq_class>(p.x()) );
+	std::cerr << "y=";
+	ratss_debug_print_mpq_class( convert<mpq_class>(p.y()) );
+	std::cerr << "z=";
+	ratss_debug_print_mpq_class( convert<mpq_class>(p.z()) );
 	std::cerr << ")" << std::endl;
 }
 
 
 void dts2_debug_print_kernel_sp_point(LIB_DTS2_NAMESPACE::Kernel_sp::Point_2 const & p) {
 	using ratss::convert;
-	std::cerr << "Point_sp(num0=" << p.numerator0() << ", num1" << p.numerator1() << ", den=" << p.denominator() << ")=";
+	std::cerr << "Point_sp(" << p.numerator0() << ", " << p.numerator1() << ", " << p.denominator() << ", " << p.pos() << ")=";
 	std::cerr << "Point_3(" << std::endl;
 	std::cerr << "x=";
 	ratss_debug_print_mpq_class( convert<mpq_class>(p.x()) );
+	std::cerr << "y=";
+	ratss_debug_print_mpq_class( convert<mpq_class>(p.y()) );
+	std::cerr << "z=";
+	ratss_debug_print_mpq_class( convert<mpq_class>(p.z()) );
 	std::cerr << ")" << std::endl;
 }
 
 void dts2_debug_print_kernel_sp64_point(LIB_DTS2_NAMESPACE::Kernel_sp_64::Point_2 const & p) {
 	using ratss::convert;
-	std::cerr << "Point_sp(num0=" << p.numerator0() << ", num1" << p.numerator1() << ", den=" << p.denominator() << ")=";
+	std::cerr << "Point_sp(" << p.numerator0() << ", " << p.numerator1() << ", " << p.denominator() << ", " << p.pos() << ")=";
 	std::cerr << "Point_3(" << std::endl;
 	std::cerr << "x=";
 	ratss_debug_print_mpq_class( convert<mpq_class>(p.x()) );
+	std::cerr << "y=";
+	ratss_debug_print_mpq_class( convert<mpq_class>(p.y()) );
+	std::cerr << "z=";
+	ratss_debug_print_mpq_class( convert<mpq_class>(p.z()) );
 	std::cerr << ")" << std::endl;
 }
 
