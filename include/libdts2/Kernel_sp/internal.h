f82a8cf5d39b3dc644dbb038150b4b43d68f7c17
Daniel Bahrdt <daniel.git@funroll-loops.de>
2020-08-29 22:18:41 +0200

    AutoInt: use uint64_t for upper bound

--- include/libdts2/Kernel_sp/internal.h
+++ include/libdts2/Kernel_sp/internal.h
@@ -257,17 +257,17 @@ struct Numerators_3 {
 	Numerators_3 & operator=(Numerators_3 &&) = default;
 };
 
-constexpr uint64_t log2(uint64_t v) {
+constexpr uint64_t minNeededBits(uint64_t v) {
 	if (v == 0 || v == 1) {
 		return 1;
 	}
 	else {
-		return 1+log2(v/2);
+		return 1+minNeededBits(v/2);
 	}
 }
 
-constexpr std::pair<const int, const bool> autoint_add_upper_bound(const int up1, const bool upl1, const int up2, const bool upl2) {
-	using return_type = std::pair<const int, const bool>;
+constexpr std::pair<const uint64_t, const bool> autoint_add_upper_bound(const uint64_t up1, const bool upl1, const uint64_t up2, const bool upl2) {
+	using return_type = std::pair<const uint64_t, const bool>;
 	if (upl1 && upl2) {
 		return return_type(std::max(up1, up2)+1, true);
 	}
@@ -276,14 +276,14 @@ constexpr std::pair<const int, const bool> autoint_add_upper_bound(const int up1
 			return return_type(up1+up2, false);
 		}
 		else {
-			return return_type(log2(uint64_t(up1)+uint64_t(up2)), true);
+			return return_type(minNeededBits(up1+up2), true);
 		}
 	}
 	else if (upl1 && !upl2) {
 		if (up1 < 63) {
 			const uint64_t up1_no_log = (static_cast<uint64_t>(1) << up1)-1;
-			const uint64_t result_up_no_log = up1_no_log + uint64_t(up2);
-			if (result_up_no_log < up1_no_log) { //overflow
+			const uint64_t result_up_no_log = up1_no_log + up2;
+			if (result_up_no_log < up1_no_log) { //overflow should not happen
 				return return_type(up1+1, true);
 			}
 			else {
@@ -291,7 +291,7 @@ constexpr std::pair<const int, const bool> autoint_add_upper_bound(const int up1
 					return return_type(result_up_no_log, false);
 				}
 				else {
-					return return_type(log2(result_up_no_log), true);
+					return return_type(minNeededBits(result_up_no_log), true);
 				}
 			}
 		}
@@ -304,8 +304,8 @@ constexpr std::pair<const int, const bool> autoint_add_upper_bound(const int up1
 	}
 }
 
-constexpr std::pair<const int, const bool> autoint_mult_upper_bound(const int up1, const bool upl1, const int up2, const bool upl2) {
-	using return_type = std::pair<const int, const bool>;
+constexpr std::pair<const uint64_t, const bool> autoint_mult_upper_bound(const uint64_t up1, const bool upl1, const uint64_t up2, const bool upl2) {
+	using return_type = std::pair<const uint64_t, const bool>;
 	if (upl1 && upl2) {
 		return return_type(up1+up2, true);
 	}
@@ -314,27 +314,27 @@ constexpr std::pair<const int, const bool> autoint_mult_upper_bound(const int up
 			return return_type(up1*up2, false);
 		}
 		else {
-			return return_type(log2(uint64_t(up1)*uint64_t(up2)), true);
+			return return_type(minNeededBits(up1*up2), true);
 		}
 	}
 	else if (upl1 && !upl2) {
 		if (up1 < 63) { //up2 is at most 2^31-1 thus we can detect an overflow
 			const uint64_t up1_no_log = (static_cast<uint64_t>(1) << up1)-1;
-			const uint64_t result_up_no_log = up1_no_log * uint64_t(up2);
+			const uint64_t result_up_no_log = up1_no_log * up2;
 			if (result_up_no_log < up1_no_log) { //overflow
-				return return_type(up1+log2(up2), true);
+				return return_type(up1+minNeededBits(up2), true);
 			}
 			else {
 				if (result_up_no_log <= std::numeric_limits<int32_t>::max()) {
 					return return_type(result_up_no_log, false);
 				}
 				else {
-					return return_type(log2(result_up_no_log), true);
+					return return_type(minNeededBits(result_up_no_log), true);
 				}
 			}
 		}
 		else {
-			return return_type(up1+log2(up2), true);
+			return return_type(up1+minNeededBits(up2), true);
 		}
 	}
 	else { // (!upl1 && upl2)
@@ -343,10 +343,10 @@ constexpr std::pair<const int, const bool> autoint_mult_upper_bound(const int up
 }
 
 ///Upper bound is an absolute value
-template<int T_UPPER_BOUND, bool T_UB_LOG = true, template<int> typename T_UNDERLYING_TYPE = AINT>
+template<uint64_t T_UPPER_BOUND, bool T_UB_LOG = true, template<int> typename T_UNDERLYING_TYPE = AINT>
 class AutoInt {
 public:
-	static constexpr int digits = (T_UB_LOG ? T_UPPER_BOUND : log2(T_UPPER_BOUND));
+	static constexpr int digits = (T_UB_LOG ? T_UPPER_BOUND : minNeededBits(T_UPPER_BOUND));
 	using underlying_type = T_UNDERLYING_TYPE<digits>;
 public:
 	AutoInt() {}
@@ -356,12 +356,12 @@ public:
 	AutoInt(underlying_type const & v) : m_v(v) {}
 	AutoInt(underlying_type && v) : m_v(std::move(v)) {}
 public:
-	template<int T_O_UPPER_BOUND, bool T_O_UB_LOG>
+	template<uint64_t T_O_UPPER_BOUND, bool T_O_UB_LOG>
 	AutoInt & operator=(
 		typename std::enable_if<
 			(T_O_UB_LOG == T_UB_LOG && T_O_UPPER_BOUND <= T_UPPER_BOUND) ||
-			(T_O_UB_LOG && !T_UB_LOG && T_O_UPPER_BOUND < log2(T_UPPER_BOUND)) ||
-			(!T_O_UB_LOG && T_UB_LOG && log2(T_O_UPPER_BOUND) <= T_UPPER_BOUND),
+			(T_O_UB_LOG && !T_UB_LOG && T_O_UPPER_BOUND < minNeededBits(T_UPPER_BOUND)) ||
+			(!T_O_UB_LOG && T_UB_LOG && minNeededBits(T_O_UPPER_BOUND) <= T_UPPER_BOUND),
 			AutoInt<T_O_UPPER_BOUND, T_O_UB_LOG, T_UNDERLYING_TYPE>
 		>::type const & other)
 	{
@@ -373,28 +373,28 @@ public:
 		return *this;
 	}
 public:
-	template<int T_O_UPPER_BOUND, bool T_O_UB_LOG>
+	template<uint64_t T_O_UPPER_BOUND, bool T_O_UB_LOG>
 	friend auto
 	operator+(AutoInt<T_UPPER_BOUND, T_UB_LOG, T_UNDERLYING_TYPE> const & me, AutoInt<T_O_UPPER_BOUND, T_O_UB_LOG, T_UNDERLYING_TYPE> const & other) {
-		constexpr std::pair<const int, const bool> result_traits = autoint_add_upper_bound(T_UPPER_BOUND, T_UB_LOG, T_O_UPPER_BOUND, T_O_UB_LOG);
+		constexpr std::pair<const uint64_t, const bool> result_traits = autoint_add_upper_bound(T_UPPER_BOUND, T_UB_LOG, T_O_UPPER_BOUND, T_O_UB_LOG);
 		using return_type = AutoInt<result_traits.first, result_traits.second, T_UNDERLYING_TYPE>;
 		using return_underlying_type = typename return_type::underlying_type;
 		return return_type( ipt_static_cast<return_underlying_type>(me.value()) + ipt_static_cast<return_underlying_type>(other.value()) );
 	}
 
-	template<int T_O_UPPER_BOUND, bool T_O_UB_LOG>
+	template<uint64_t T_O_UPPER_BOUND, bool T_O_UB_LOG>
 	friend auto
 	operator-(AutoInt<T_UPPER_BOUND, T_UB_LOG, T_UNDERLYING_TYPE> const & me, AutoInt<T_O_UPPER_BOUND, T_O_UB_LOG, T_UNDERLYING_TYPE> const & other) {
-		constexpr std::pair<const int, const bool> result_traits = autoint_add_upper_bound(T_UPPER_BOUND, T_UB_LOG, T_O_UPPER_BOUND, T_O_UB_LOG);
+		constexpr std::pair<const uint64_t, const bool> result_traits = autoint_add_upper_bound(T_UPPER_BOUND, T_UB_LOG, T_O_UPPER_BOUND, T_O_UB_LOG);
 		using return_type = AutoInt<result_traits.first, result_traits.second, T_UNDERLYING_TYPE>;
 		using return_underlying_type = typename return_type::underlying_type;
 		return return_type( ipt_static_cast<return_underlying_type>(me.value()) - ipt_static_cast<return_underlying_type>(other.value()) );
 	}
 	
-	template<int T_O_UPPER_BOUND, bool T_O_UB_LOG>
+	template<uint64_t T_O_UPPER_BOUND, bool T_O_UB_LOG>
 	friend auto
 	operator*(AutoInt<T_UPPER_BOUND, T_UB_LOG, T_UNDERLYING_TYPE> const & me, AutoInt<T_O_UPPER_BOUND, T_O_UB_LOG, T_UNDERLYING_TYPE> const & other) {
-		constexpr std::pair<const int, const bool> result_traits = autoint_mult_upper_bound(T_UPPER_BOUND, T_UB_LOG, T_O_UPPER_BOUND, T_O_UB_LOG);
+		constexpr std::pair<const uint64_t, const bool> result_traits = autoint_mult_upper_bound(T_UPPER_BOUND, T_UB_LOG, T_O_UPPER_BOUND, T_O_UB_LOG);
 		using return_type = AutoInt<result_traits.first, result_traits.second, T_UNDERLYING_TYPE>;
 		using return_underlying_type = typename return_type::underlying_type;
 		return return_type( ipt_static_cast<return_underlying_type>(me.value()) * ipt_static_cast<return_underlying_type>(other.value()) );
@@ -412,7 +412,7 @@ private:
 
 namespace LIB_RATSS_NAMESPACE {
 	
-template<int T_UPPER_BOUND, bool T_UB_LOG, template<int> typename T_UNDERLYING_TYPE>
+template<uint64_t T_UPPER_BOUND, bool T_UB_LOG, template<int> typename T_UNDERLYING_TYPE>
 struct Conversion< LIB_DTS2_NAMESPACE::detail::Kernel_sp::AutoInt<T_UPPER_BOUND, T_UB_LOG, T_UNDERLYING_TYPE> > {
 	using type = LIB_DTS2_NAMESPACE::detail::Kernel_sp::AutoInt<T_UPPER_BOUND, T_UB_LOG, T_UNDERLYING_TYPE>;
 	using underlying_type = typename type::underlying_type;
