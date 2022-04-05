7d62149f0dff73c11bac1a88ca4a83b0cf0acab6
Daniel Bahrdt <daniel.git@funroll-loops.de>
2022-04-05 17:34:02 +0200

    Improve debug functions

--- src/debug.cpp
+++ src/debug.cpp
@@ -6,7 +6,7 @@ extern "C" {
 	
 void dts2_debug_print_point_sp(LIB_DTS2_NAMESPACE::Constrained_delaunay_triangulation_with_inexact_intersections_traits_s2_sp::Point_2 const & p) {
 	using ratss::convert;
-	std::cerr << "Point_sp(" << p.numerator0() << ", " << p.numerator1() << ", " << p.denominator() << ", " << p.pos() << ")=";
+	std::cerr << "Point_sp(num0=" << p.numerator0() << ", num1=" << p.numerator1() << ", den=" << p.denominator() << ", pos=" << p.pos() << ")=";
 	std::cerr << "Point_3(" << std::endl;
 	std::cerr << "x=";
 	ratss_debug_print_mpq_class( convert<mpq_class>(p.x()) );
@@ -20,7 +20,7 @@ void dts2_debug_print_point_sp(LIB_DTS2_NAMESPACE::Constrained_delaunay_triangul
 
 void dts2_debug_print_kernel_sp_point(LIB_DTS2_NAMESPACE::Kernel_sp::Point_2 const & p) {
 	using ratss::convert;
-	std::cerr << "Point_sp(" << p.numerator0() << ", " << p.numerator1() << ", " << p.denominator() << ", " << p.pos() << ")=";
+	std::cerr << "Point_sp(num0=" << p.numerator0() << ", num1=" << p.numerator1() << ", den=" << p.denominator() << ", pos=" << p.pos() << ")=";
 	std::cerr << "Point_3(" << std::endl;
 	std::cerr << "x=";
 	ratss_debug_print_mpq_class( convert<mpq_class>(p.x()) );
@@ -33,7 +33,7 @@ void dts2_debug_print_kernel_sp_point(LIB_DTS2_NAMESPACE::Kernel_sp::Point_2 con
 
 void dts2_debug_print_kernel_sp64_point(LIB_DTS2_NAMESPACE::Kernel_sp_64::Point_2 const & p) {
 	using ratss::convert;
-	std::cerr << "Point_sp(" << p.numerator0() << ", " << p.numerator1() << ", " << p.denominator() << ", " << p.pos() << ")=";
+	std::cerr << "Point_sp(num0=" << p.numerator0() << ", num1=" << p.numerator1() << ", den=" << p.denominator() << ", pos=" << p.pos() << ")=";
 	std::cerr << "Point_3(" << std::endl;
 	std::cerr << "x=";
 	ratss_debug_print_mpq_class( convert<mpq_class>(p.x()) );
@@ -48,10 +48,10 @@ void dts2_debug_print_kernel_sp64_point(LIB_DTS2_NAMESPACE::Kernel_sp_64::Point_
 
 namespace LIB_DTS2_NAMESPACE {
 	
-#define PRINT_FN_ADDR(__F) std::cout << std::size_t(&__F);
+#define PRINT_FN_ADDR(__F) std::cout << #__F ": " << std::size_t(&__F) << std::endl;
 void init_interactive_debuging() {
 	ratss::init_interactive_debuging();
-	std::cout << "Initializing interactive debugging functionality ";
+	std::cout << "Initializing interactive debugging functionality of dts2:\n";
 	PRINT_FN_ADDR(dts2_debug_print_point_sp)
 	PRINT_FN_ADDR(dts2_debug_print_kernel_sp_point)
 	PRINT_FN_ADDR(dts2_debug_print_kernel_sp64_point)
